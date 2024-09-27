ae443e48134323ef2a87ef085554be7f91b070f4
Leonardo Sobral <leonardo98barros@gmail.com>
2024-09-27 14:00:23 -0300

    Aula sobre loops

---
+++ src/components/MyButton.vue
@@ -0,0 +1,25 @@
+<template>
+     <button @click="handleClick">{{ label }}</button>
+</template>
+
+<script>
+    export default {
+  props: ['label'],
+  methods: {
+    handleClick() {
+      console.log('Button clicked!');
+    }
+  }
+};
+</script>
+
+<style>
+button {
+  padding: 10px 20px;
+  background-color: #3498db;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
